f87909c98fa0aecb63ca0c5d404f73f1bd794780
Jarosław Bąk <13832590+bonsoirq@users.noreply.github.com>
2024-08-29 21:15:39 +0200

    chore: adjust arrow keys

--- keyboards/sofle/keymaps/rgb_bonsoirq_via/keymap.c
+++ keyboards/sofle/keymaps/rgb_bonsoirq_via/keymap.c
@@ -100,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,     KC_1,   KC_2,       KC_3,       KC_4,       KC_5,                               KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_GRV,
     KC_TAB,     KC_Q,   KC_W,       KC_E,       KC_R,       KC_T,                               KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSPC,
     KC_BSPC,    KC_A,   KC_S,       KC_D,       KC_F,       KC_G,                               KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
-    KC_LSFT,    KC_Z,   KC_X,       KC_C,       KC_V,       KC_B,       KC_MUTE,    XXXXXXX,  KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,
+    KC_LSFT,    KC_Z,   KC_X,       KC_C,       KC_V,       KC_B,       KC_MUTE,    XXXXXXX,    KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,
                         KC_LCTL,    KC_LGUI,    KC_LALT,    KC_LOWER,   KC_SPC,     KC_ENT,     KC_RAISE,   KC_RALT,    KC_RCTL,    KC_ADJUST
 ),
 
@@ -143,7 +143,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT(
     _______,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,                           KC_F6,     KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,
-    KC_GRV,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                            KC_6,      KC_7,       KC_8,       KC_9,       KC_0,       KC_F12,
+    _______,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                            KC_6,      KC_7,       KC_8,       KC_9,       KC_0,       KC_F12,
     _______,    KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC,                         KC_CIRC,   KC_AMPR,    KC_ASTR,    KC_LPRN,    KC_RPRN,    KC_PIPE,
     _______,    KC_EQL,    KC_MINS,     KC_PLUS,    KC_LCBR,    KC_RCBR, _______,     _______,   KC_LBRC,   KC_RBRC,    KC_UNDS,    KC_QUES,    KC_BSLS,    _______,
                             _______,    _______,    _______,    _______, _______,     _______,   _______,   _______,    _______,    _______
@@ -163,9 +163,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_RAISE] = LAYOUT(
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                            XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,   XXXXXXX,     XXXXXXX,
+    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                            XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
     XXXXXXX,    KC_INS,     KC_PSCR,    KC_APP,     XXXXXXX,    XXXXXXX,                            XXXXXXX,    KC_MPRV,    KC_MPLY,    KC_MNXT,    XXXXXXX,    XXXXXXX,
-    XXXXXXX,    KC_LALT,    KC_LCTL,    KC_LSFT,    XXXXXXX,    KC_CAPS,                            KC_LEFT,    KC_UP,      KC_DOWN,    KC_RGHT,    XXXXXXX,    XXXXXXX,
+    XXXXXXX,    KC_LALT,    KC_LCTL,    KC_LSFT,    XXXXXXX,    KC_CAPS,                            KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT,    XXXXXXX,    XXXXXXX,
     XXXXXXX,    KC_UNDO,    KC_CUT,     KC_COPY,    KC_PASTE,   XXXXXXX,    _______,    _______,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
                             _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______
 ),
@@ -232,8 +232,7 @@ const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
 
 layer_state_t layer_state_set_user(layer_state_t state) {
 	rgblight_set_layer_state(0, layer_state_cmp(state, _DEFAULTS) && layer_state_cmp(default_layer_state,_QWERTY));
-	rgblight_set_layer_state(7, layer_state_cmp(state, _DEFAULTS) && layer_state_cmp(default_layer_state,_COLEMAKDH));
-
+	rgblight_set_layer_state(4, layer_state_cmp(state, _DEFAULTS) && layer_state_cmp(default_layer_state,_COLEMAKDH));
 
 	rgblight_set_layer_state(1, layer_state_cmp(state, _LOWER));
 	rgblight_set_layer_state(2, layer_state_cmp(state, _RAISE));
